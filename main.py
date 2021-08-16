b0f045f75ed5ae67992bc26023eaf9f698ba1529
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-08-16 15:20:36 +0300

    Update main.py
    Show video

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import cv2
 
+# How to show image
 image = cv2.imread('images/profile.jpg')
 cv2.imshow('Profile image', image)
 
 cv2.waitKey(0)
+
+# How to show video
+cap = cv2.VideoCapture('videos/path_to_video.mp4')
+
+while True:
+    success, image = cap.read()
+    cv2.imshow('Result', image)
+
+    if cv2.waitKey(1) and ord('q') == 0xFF:
+        break
