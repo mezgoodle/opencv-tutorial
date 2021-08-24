fcaf8288196b1b6fcdc75a605868097318f7ed50
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-08-24 12:48:11 +0300

    Update transform_images.py
    SHow image

--- transform_images.py
+++ transform_images.py
@@ -1,22 +1,9 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('video.mp4')
+image = cv2.imread('images/profile.jpg')
 
-while True:
-    success, image = cap.read()
+image = cv2.flip(image, 1)
+cv2.imshow('Result', image)
 
-    new_image = cv2.resize(image, (300, 500))
-    new_image = cv2.GaussianBlur(new_image, (9, 9), 0)
-    new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
-    new_image = cv2.Canny(new_image, 100, 100)
-    kernel = np.ones((5, 5), np.uint8)
-    new_image = cv2.dilate(new_image, kernel, iterations=1)
-    new_image = cv2.erode(new_image, kernel, iterations=1)
-    cv2.imshow('Profile image', image)
-    cv2.imshow('Profile image', new_image[0:100, 0:150])
-
-    cv2.imshow('Result', image)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+cv2.waitKey(0)
