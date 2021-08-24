7d015853c2a87e6037f597e93f219143f53b7719
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-08-24 13:03:43 +0300

    Update contour.py
    Draw image by contour

--- contour.py
+++ contour.py
@@ -3,6 +3,7 @@ import numpy as np
 
 # How to show image
 image = cv2.imread('images/profile.jpg')
+new_image = np.zeros(image.shape, dtype='uint8')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 image = cv2.GaussianBlur(image, (9, 9), 0)
 
@@ -10,8 +11,9 @@ image = cv2.Canny(image, 100, 140)
 
 con, hir = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
-print(con)
+# print(con)
 
+cv2.drawContours(new_image, con, -1, (230, 111, 148), 1)
 
-cv2.imshow('Profile image', image)
+cv2.imshow('Profile image', new_image)
 cv2.waitKey(0)
