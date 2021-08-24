ce1ff7999d4e9c73511aed5a836eb3ede2501370
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-08-24 12:52:15 +0300

    Update transform_images.py
    Rotate image

--- transform_images.py
+++ transform_images.py
@@ -3,7 +3,18 @@ import numpy as np
 
 image = cv2.imread('images/profile.jpg')
 
-image = cv2.flip(image, 1)
+
+# image = cv2.flip(image, 1)
+
+def rotate(image, angle: int):
+    height, width = image.shape[:2]
+    point = (width // 2, height // 2)
+
+    matrix = cv2.getRotationMatrix2D(point, angle, 1)
+    return cv2.warpAffine(image, matrix, (width, height))
+
+
+image = rotate(image, 90)
 cv2.imshow('Result', image)
 
 cv2.waitKey(0)
