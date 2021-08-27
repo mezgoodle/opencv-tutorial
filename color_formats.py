21e0844bb4a7e7c557918389428cd5acf291e330
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-08-27 09:09:05 +0300

    Update color_formats.py
    Convert to LAB

--- color_formats.py
+++ color_formats.py
@@ -2,7 +2,7 @@ import cv2
 
 image = cv2.imread('images/profile.jpg')
 
-image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+image = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
 
 cv2.imshow('Result', image)
 cv2.waitKey(0)
