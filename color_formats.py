6a0e36c9cc11aae6869415570fbb758bfb9df4bc
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-08-27 09:11:24 +0300

    Update color_formats.py
    Show lawyers

--- color_formats.py
+++ color_formats.py
@@ -4,6 +4,10 @@ image = cv2.imread('images/profile.jpg')
 
 image = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
 
-cv2.imshow('Result', image)
+image = cv2.cvtColor(image, cv2.COLOR_LAB2BGR)
+
+b, g, r = cv2.split(image)
+
+cv2.imshow('Result', b)
 cv2.waitKey(0)
 
