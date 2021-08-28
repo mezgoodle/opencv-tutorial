f68e36a64dd1cac6a53930fd384beeca4726dbc4
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-08-28 11:35:12 +0300

    Update byte_operations.py
    Create mask

--- byte_operations.py
+++ byte_operations.py
@@ -1,15 +1,16 @@
 import cv2
 import numpy as np
 
-image = np.zeros((350, 350), dtype='uint8')
+photo = cv2.imread('images/profile.jpg')
+image = np.zeros(photo.shape[:2], dtype='uint8')
 
-circle = cv2.circle(image.copy(), (0, 0), 80, 255, -1)
+circle = cv2.circle(image.copy(), (200, 300), 120, 255, -1)
 square = cv2.rectangle(image.copy(), (25, 25), (250, 350), 255, -1)
 
-# image = cv2.bitwise_and(circle, square)
+image = cv2.bitwise_and(photo, photo, mask=circle)
 # image = cv2.bitwise_or(circle, square)
 # image = cv2.bitwise_xor(circle, square)
-image = cv2.bitwise_not(circle)
+# image = cv2.bitwise_not(circle)
 
 cv2.imshow('Result', image)
 cv2.waitKey(0)
