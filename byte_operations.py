ea9fc4c4052b4119cce158d694fa49a5fdfc00db
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-08-28 11:33:29 +0300

    Update byte_operations.py
    Byte operations

--- byte_operations.py
+++ byte_operations.py
@@ -6,5 +6,10 @@ image = np.zeros((350, 350), dtype='uint8')
 circle = cv2.circle(image.copy(), (0, 0), 80, 255, -1)
 square = cv2.rectangle(image.copy(), (25, 25), (250, 350), 255, -1)
 
-cv2.imshow('Result', circle)
+# image = cv2.bitwise_and(circle, square)
+# image = cv2.bitwise_or(circle, square)
+# image = cv2.bitwise_xor(circle, square)
+image = cv2.bitwise_not(circle)
+
+cv2.imshow('Result', image)
 cv2.waitKey(0)
