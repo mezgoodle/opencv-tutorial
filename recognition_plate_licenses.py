89bcbea9cdad0b52d342c70cf5ddb8da784ee43d
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-02 16:07:11 +0300

    Optimize image: add filter and edges
    I added bilateralFilter, that applies bilateral filtering to the input image and found edges by method Canny

--- recognition_plate_licenses.py
+++ recognition_plate_licenses.py
@@ -6,5 +6,9 @@ from matplotlib import pyplot as plt
 
 image = cv2.imread('images/license.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+image_filter = cv2.bilateralFilter(gray, 11, 15, 15)
+edges = cv2.Canny(image_filter, 30, 200)
+
+plt.imshow(cv2.cvtColor(edges, cv2.COLOR_BGR2RGB))
 plt.show()
