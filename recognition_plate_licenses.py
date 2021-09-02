91b66ffb96f12dd843e47f093a34761d7fa9b17e
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-02 16:14:38 +0300

    Find contours: find contours by opencv and choose necessery by imutils
    I found contours by opencv in a hierarchical order. Then grab them by imutils and sorted by built-in method in python

--- recognition_plate_licenses.py
+++ recognition_plate_licenses.py
@@ -10,5 +10,9 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 image_filter = cv2.bilateralFilter(gray, 11, 15, 15)
 edges = cv2.Canny(image_filter, 30, 200)
 
+contours = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(contours)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:8]
+
 plt.imshow(cv2.cvtColor(edges, cv2.COLOR_BGR2RGB))
 plt.show()
