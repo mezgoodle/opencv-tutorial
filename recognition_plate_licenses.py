25f33a6866dee32f346008dcdc021d83a5b8d99b
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-02 16:35:24 +0300

    Optimize show: search mask and add white background
    By NumPy I searched coordinates of edges

--- recognition_plate_licenses.py
+++ recognition_plate_licenses.py
@@ -12,7 +12,7 @@ edges = cv2.Canny(image_filter, 30, 200)
 
 contours = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(contours)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)[:8]
+contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
 position = None
 for contour in contours:
@@ -26,5 +26,10 @@ mask = np.zeros(gray.shape, np.uint8)
 new_image = cv2.drawContours(mask, [position], 0, 255, -1)
 bitwise_image = cv2.bitwise_and(image, image, mask=mask)
 
-plt.imshow(cv2.cvtColor(edges, cv2.COLOR_BGR2RGB))
+x, y = np.where(mask == 255)
+x1, y1 = np.min(x), np.min(y)
+x2, y2 = np.max(x), np.max(y)
+crop = gray[x1:x2, y1:y2]
+
+plt.imshow(cv2.cvtColor(crop, cv2.COLOR_BGR2RGB))
 plt.show()
