d0041d63e5eabc627138a81939ff1b7d547d9e7b
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-09-02 16:20:42 +0300

    Search rectangle: find rectangle from contours
    I went through contours and tried to do rectangle. If I success, I shall break the loop

--- recognition_plate_licenses.py
+++ recognition_plate_licenses.py
@@ -14,5 +14,15 @@ contours = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
 contours = imutils.grab_contours(contours)
 contours = sorted(contours, key=cv2.contourArea, reverse=True)[:8]
 
+position = None
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 10, True)
+    number_of_edges = 4
+    if len(approx) == number_of_edges:
+        position = approx
+        break
+
+print(position)
+
 plt.imshow(cv2.cvtColor(edges, cv2.COLOR_BGR2RGB))
 plt.show()
